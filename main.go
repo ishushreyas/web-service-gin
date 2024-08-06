410b850bdf11c4f50c633a58ca0ac55d4a06fc1e
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-08-06 15:43:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,51 +6,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type version struct {
+	ID string `json:"id"`
+	Url string `json:"Url"`
+	Contributor string `json:"contributor"`
+	Version float64 `json:"Version"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var versions = []album{
+	{ID: "1", Url: "Blue Train", Contributor: "John Coltrane", Version: 1.0},
+	{ID: "2", Url: "Jeru", Contributor: "Gerry Mulligan", Version: 1.001},
+	{ID: "3", Url: "Sarah Vaughan and Clifford Brown", Contributor: "Sarah Vaughan", Version: 1.002},
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getVersions(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, version)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func postVersions(c *gin.Context) {
+	var newVersion version 
 
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newVersion); err != nil {
 		return
 	}
 
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	versions = append(versions, newVersion)
+	c.IndentedJSON(http.StatusCreated, newVersion)
 }
 
-func getAlbumByID(c *gin.Context) {
+func getVersionByID(c *gin.Context) {
 	id := c.Param("id")
 
-	for _, a := range albums {
+	for _, a := range versions {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "version not found"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/versions", getVersionss)
+	router.GET("/version/:id", getVersionByID)
+	router.POST("/versions", postVersions)
 
 	router.Run("0.0.0.0:8080")
 }
